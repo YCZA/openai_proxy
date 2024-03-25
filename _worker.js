a415347d9cf19ad70143f7d30f61fc8dda8f1458
AidenChester <93750234+YCZA@users.noreply.github.com>
2024-03-26 02:01:52 +0800

    fix bug

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const targetHost = url.searchParams.get('target') || 'api.openai.com';
-    url.host = "api.openai.com";
+    url.host = targetHost;
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,

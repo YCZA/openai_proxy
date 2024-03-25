465507ef854393a5b53cc9377c80dfcb0540c160
AidenChester <93750234+YCZA@users.noreply.github.com>
2024-03-26 01:50:40 +0800

    添加target参数

--- _worker.js
+++ _worker.js
@@ -1,6 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    const targetHost = url.searchParams.get('target') || 'api.openai.com';
     url.host = "api.openai.com";
     // openai is already set all CORS heasders 
     return fetch(url, {
